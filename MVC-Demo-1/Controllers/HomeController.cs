eb6d4e09f6c6583e5a56056e6886dcfd0212109e
Mohd Naved Alam <mohdnavedalam87@gmail.com>
2021-01-29 00:05:59 +0530

    Views and Action Methods

--- MVC-Demo-1/Controllers/HomeController.cs
+++ MVC-Demo-1/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System.Web.Mvc;
+﻿using System.Linq;
+using System.Web.Mvc;
 using MVC_Demo_1.Models;
 
 namespace MVC_Demo_1.Controllers
@@ -25,6 +26,27 @@ namespace MVC_Demo_1.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        public ActionResult Details(int id)
+        {
+            var selectedDepartment = db.Departments.Single(d => d.Id == id);
+
+            ViewBag.Message = "Details";
+            return View(selectedDepartment);
+        }
+
+        public ActionResult Create()
+        {
+            return View(new Department());
+        }
+
+        [HttpPost]
+        public ActionResult Create(Department department)
+        {
+            db.Departments.Add(department);
+            db.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
